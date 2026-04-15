f9710bbe6bbe5060165e75eaa9a46ad24da094e3
Pasha Mir <pavmiryar@gmail.com>
2026-04-15 13:15:07 +0300

    function to add new records

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # страница для добавления новой темы
     path('new_topic/', views.new_topic, name='new_topic'),
+    # страница для добавления новой записи в тему
+    path('new_entry/{int:topic_id}/', views.new_entry, name='new_entry'),
 ]
