94d8bff6b094e30e8ca03882e31bedc7ecdbd22f
Pasha Mir <pavmiryar@gmail.com>
2026-04-15 16:51:57 +0300

    registration function

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """определяет схемы URL для пользователей"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # включить URL авторизации по умолчанию
     path('', include('django.contrib.auth.urls')),
+    # страница регистрации
+    path('register/', views.register, name='register'),
 ]
