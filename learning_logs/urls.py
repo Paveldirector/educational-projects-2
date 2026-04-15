70e2b8e4a7acca6da8cd72f446702e602fc06a49
Pasha Mir <pavmiryar@gmail.com>
2026-04-15 10:54:46 +0300

    syntaxerror

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # домашняя страница
     path('', views.index, name='index'),
     # страница со списком всех тем
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
     # страница с подробной информацией по отдельной теме
-    path('topics/<int:topic_id>/', views.topics, name='topics')
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
